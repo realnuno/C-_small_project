c444b76e02bd2322492d4a2840c9e25691a4808a
Ianhwang <inbeom918@gmail.com>
2021-09-21 14:19:31 -0400

    finished Movie app

---
+++ Movie.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Movie.h"
+
+using namespace std;
+
+Movie::Movie(string name, string rating, int watched)
+	:name(name), rating(rating), watched(watched) {
+};
+
+Movie::Movie(const Movie& source)
+	:Movie{ source.name, source.rating, source.watched } {
+
+};
+
+Movie::~Movie() {
+};
+
+void Movie::display() const {
+	cout << name << ", " << rating << ", " << watched << endl;
+}
---
+++ Movie.h
@@ -0,0 +1,34 @@
+#ifndef _MOVIE_H_
+#define _MOVIE_H_
+
+#include <string>
+
+using namespace std;
+
+class Movie
+{
+private:
+	string name;
+	string rating;
+	int watched;
+
+public:
+	Movie(string name, string rating, int watched);
+	Movie(const Movie& source);
+	~Movie();
+
+	void set_name(string name) { this->name = name; };
+	string get_name() const { return name; }
+
+	void set_rating(string rating) { this->rating = rating; };
+	string get_rating() const { return rating; };
+
+	void set_watched() { this->watched = watched; };
+	int get_watched() const { return watched; };
+
+	void increment_watched() { ++watched; };
+
+	void display() const;
+};
+
+#endif // !_MOVIE_H_
